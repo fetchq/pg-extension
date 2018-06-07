0eaa5951f1be4fbc5ba8c4ba9f580e638bf21842
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-06-07 17:34:18 +0200

    Add basic assertion capabilities to the query

--- tests/create-drop-queue.sql
+++ tests/create-drop-queue.sql
@@ -1,7 +1,31 @@
-create extension fetchq;
-select * from fetchq_create_queue('foo');
-select * from fetchq__foo__documents;
-select * from fetchq__foo__metrics;
-select * from fetchq__foo__errors;
-select * from fetchq_drop_queue('foo');
-drop extension fetchq;
+
+
+DROP FUNCTION IF EXISTS fetchq_create_drop_queue_test();
+CREATE OR REPLACE FUNCTION fetchq_create_drop_queue_test (
+    OUT passed BOOLEAN
+) AS $$
+DECLARE
+	VAR_numDocs INTEGER;
+BEGIN
+    CREATE EXTENSION fetchq;
+    PERFORM fetchq_create_queue('foo');
+
+    -- check basic tables
+    PERFORM * FROM fetchq__foo__documents;
+    PERFORM * FROM fetchq__foo__metrics;
+    PERFORM * FROM fetchq__foo__errors;
+
+    -- check jobs table
+    SELECT COUNT(*) INTO VAR_numDocs FROM fetchq_sys_jobs WHERE subject = 'foo';
+    IF VAR_numDocs < 4 THEN
+		RAISE EXCEPTION 'wrong expectation';
+	END IF;
+
+    PERFORM fetchq_drop_queue('foo');
+    DROP EXTENSION fetchq;
+
+    passed = TRUE;
+END; $$
+LANGUAGE plpgsql;
+
+select * from fetchq_create_drop_queue_test();
