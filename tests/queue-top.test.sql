d3cf976b7cb43243533c786d7c1adab3df40a75a
marco pegoraro <marco.pegoraro@gmail.com>
2020-09-29 10:52:01 +0200

    Move fetchq_queue_top into catalog

--- tests/queue-top.test.sql
+++ tests/queue-top.test.sql
@@ -19,17 +19,17 @@ BEGIN
     PERFORM fetchq_doc_push('foo', 'a5', 1, 0, NOW() + INTERVAL '1m', '{}');
     PERFORM fetchq_doc_push('foo', 'a6', 1, 0, NOW() + INTERVAL '1m', '{}');
     
-    SELECT COUNT(*) INTO VAR_numDocs FROM fetchq_queue_top('foo', 0, 3, 0);
+    SELECT COUNT(*) INTO VAR_numDocs FROM fetchq_catalog.fetchq_queue_top('foo', 0, 3, 0);
     IF VAR_numDocs != 3 THEN
         RAISE EXCEPTION 'failed - %(count, got %)', VAR_testName, VAR_numDocs;
     END IF;
 
-    SELECT COUNT(*) INTO VAR_numDocs FROM fetchq_queue_top('foo', 1, 2, 0);
+    SELECT COUNT(*) INTO VAR_numDocs FROM fetchq_catalog.fetchq_queue_top('foo', 1, 2, 0);
     IF VAR_numDocs <> 2 THEN
         RAISE EXCEPTION 'failed - %(limit, got %)', VAR_testName, VAR_numDocs;
     END IF;
 
-    SELECT * INTO VAR_r FROM fetchq_queue_top('foo', 1, 1, 1);
+    SELECT * INTO VAR_r FROM fetchq_catalog.fetchq_queue_top('foo', 1, 1, 1);
     IF VAR_r.subject <> 'a5' THEN
         RAISE EXCEPTION 'failed - %(offset, got %)', VAR_testName, VAR_r.subject;
     END IF;
