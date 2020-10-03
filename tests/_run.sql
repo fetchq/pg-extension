b07c140485972eac7eaf511c9391620e7647ac2f
marco pegoraro <marco.pegoraro@gmail.com>
2020-10-03 09:28:30 +0200

    Regroup tests

--- tests/_run.sql
+++ tests/_run.sql
@@ -1,5 +1,23 @@
 
+CREATE OR REPLACE FUNCTION fetchq_test.__runWIP(
+    OUT done BOOLEAN
+) AS $$
+DECLARE
+    VAR_errMessage TEXT;
+BEGIN
+    BEGIN
 
+        -- >>> Run tests
+        PERFORM fetchq_test.__run('init', 'It should initialize Fetchq');
+        -- <<<
+
+    EXCEPTION WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS VAR_errMessage = MESSAGE_TEXT;
+        RAISE EXCEPTION '%', VAR_errMessage;
+    END;
+    done = TRUE;
+END; $$
+LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE FUNCTION fetchq_test.__runBasics(
@@ -13,13 +31,19 @@ BEGIN
         -- >>> Run tests
         PERFORM fetchq_test.__run('init', 'It should initialize Fetchq');
         PERFORM fetchq_test.__run('queue_create_01', '');
-        PERFORM fetchq_test.__run('queue_create_02', '');
-        PERFORM fetchq_test.__run('queue_create_03', '');
         PERFORM fetchq_test.__run('queue_create_indexes_01', '');
         PERFORM fetchq_test.__run('queue_drop_01', '');
-        PERFORM fetchq_test.__run('queue_drop_02', '');
         PERFORM fetchq_test.__run('queue_top_01', '');
         PERFORM fetchq_test.__run('queue_triggers_01', '');
+        PERFORM fetchq_test.__run('doc_push_01', '');
+        PERFORM fetchq_test.__run('doc_append_01', '');
+        PERFORM fetchq_test.__run('doc_upsert_01', '');
+        PERFORM fetchq_test.__run('doc_pick_01', '');
+        PERFORM fetchq_test.__run('doc_reschedule_01', '');
+        PERFORM fetchq_test.__run('doc_reject_01', '');
+        PERFORM fetchq_test.__run('doc_complete_01', '');
+        PERFORM fetchq_test.__run('doc_kill_01', '');
+        PERFORM fetchq_test.__run('doc_drop_01', '');
         -- <<<
 
     EXCEPTION WHEN OTHERS THEN
@@ -39,30 +63,29 @@ BEGIN
     BEGIN
 
         -- >>> Run tests
-        PERFORM fetchq_test.__run('init', 'It should initialize Fetchq');
+        PERFORM fetchq_test.__run('queue_create_02', '');
+        PERFORM fetchq_test.__run('queue_create_03', '');
         PERFORM fetchq_test.__run('queue_status_01', '');
-        PERFORM fetchq_test.__run('doc_push_01', '');
+        PERFORM fetchq_test.__run('queue_drop_02', '');
+        
         PERFORM fetchq_test.__run('doc_push_02', '');
         PERFORM fetchq_test.__run('doc_push_03', '');
-        PERFORM fetchq_test.__run('doc_append_01', '');
         PERFORM fetchq_test.__run('doc_append_02', '');
-        PERFORM fetchq_test.__run('doc_upsert_01', '');
         PERFORM fetchq_test.__run('doc_upsert_02', '');
-        PERFORM fetchq_test.__run('doc_pick_01', '');
         PERFORM fetchq_test.__run('doc_pick_02', '');
         PERFORM fetchq_test.__run('doc_pick_03', '');
         PERFORM fetchq_test.__run('doc_pick_04', '');
         PERFORM fetchq_test.__run('doc_pick_05', '');
-        PERFORM fetchq_test.__run('doc_reschedule_01', '');
+        
         PERFORM fetchq_test.__run('doc_reschedule_02', '');
-        PERFORM fetchq_test.__run('doc_reject_01', '');
+        
         PERFORM fetchq_test.__run('doc_reject_02', '');
         PERFORM fetchq_test.__run('doc_reject_03', '');
-        PERFORM fetchq_test.__run('doc_complete_01', '');
+        
         PERFORM fetchq_test.__run('doc_complete_02', '');
-        PERFORM fetchq_test.__run('doc_kill_01', '');
+        
         PERFORM fetchq_test.__run('doc_kill_02', '');
-        PERFORM fetchq_test.__run('doc_drop_01', '');
+        
 
         PERFORM fetchq_test.__run('mnt_make_pending_01', '');
         PERFORM fetchq_test.__run('mnt_reschedule_orphans_01', '');
@@ -107,8 +130,9 @@ LANGUAGE plpgsql;
 
 
 -- Define which groups to run
+select * from fetchq_test.__runWIP();
 select * from fetchq_test.__runBasics();
--- select * from fetchq_test.__runOptionals();
+select * from fetchq_test.__runOptionals();
 
 
 
